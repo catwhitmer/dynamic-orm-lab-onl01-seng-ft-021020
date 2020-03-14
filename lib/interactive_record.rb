f93bd81202c01f660d3478fb2dbe73ae81735350
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-14 14:10:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,11 +51,10 @@ class InteractiveRecord
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
-end
+  end
 
-def self.find_by(attribute)
-  sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = ?"
-  DB[:conn].execute(sql, attribute.values.first)
-end
-
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = ?"
+    DB[:conn].execute(sql, attribute.values.first)
+  end
 end
